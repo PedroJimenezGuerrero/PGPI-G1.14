b7f3b0423921a7b0b04e6555367fbf854ce534fd
pabcascom <pabcascom@alum.us.es>
2024-11-23 12:05:05 +0100

    feat: sincronizacion con los mockups

--- app/order/forms.py
+++ app/order/forms.py
@@ -5,3 +5,11 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        labels = {
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+            'email': 'Correo Electrónico',
+            'address': 'Dirección',
+            'postal_code': 'Código Postal',
+            'city': 'Ciudad',
+        }
