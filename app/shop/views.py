4ee8ad35a54b085752149f19ac27557e1f43c73a
David Godoy Fernández <davgodfer.alumn.us.es>
2024-11-24 17:57:30 +0100

    Añadido barra de busqueda, y funcionalidad en el boton de añadir cursos desde la lista

--- app/shop/views.py
+++ app/shop/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from cart.forms import CartAddProductForm
 from .models import Category, Duration, Product
 from django.contrib.auth import logout
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from cart.cart import Cart
+from cart.forms import CartAddProductForm
 
 # from django.views import generic
 
@@ -17,32 +21,24 @@ from django.contrib.auth import logout
 
 
 
-def product_list(request):
-    category_slugs = request.GET.getlist('category')
-    duration_slugs = request.GET.getlist('duration')
-    min_price = request.GET.get('min_price')
-    max_price = request.GET.get('max_price')
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
 
-    products = Product.objects.filter(available=True)
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
 
-    if category_slugs:
-        products = products.filter(category__slug__in=category_slugs)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
 
-    if duration_slugs:
-        products = products.filter(duration__slug__in=duration_slugs)
-
-    if min_price:
-        products = products.filter(price__gte=min_price)
-
-    if max_price:
-        products = products.filter(price__lte=max_price)
-
-    context = {
+    return render(request, 'shop/product/list.html', {
+        'category': category,
+        'categories': categories,
         'products': products,
-        'categories': Category.objects.all(),
-        'durations': Duration.objects.all(),
-    }
-    return render(request, 'shop/product/list.html', context)
+    })
 
 # class ProductListView(generic.ListView):
 #     template_name = 'shop/product/list.html'
@@ -83,3 +79,26 @@ def product_detail(request, id, slug):
 
 def terminos(request):
     return render(request, 'shop/terminos/terminos.html')
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override': True})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
