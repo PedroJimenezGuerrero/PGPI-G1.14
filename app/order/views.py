a399a4248b0cbe353577736e1c390f98235c77ce
David Godoy Fernández <davgodfer.alumn.us.es>
2024-11-29 20:12:20 +0100

    Añadida funcionalidad de correo y logica del pago contra reembolso Necesario volver a instalar las depedencias y hacer las migraciones ya que se han cambiado ambas cosas

--- app/order/views.py
+++ app/order/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from cart.cart import Cart
-from .models import OrderItem, Order
+from django.core.mail import EmailMessage
+from django.contrib import messages
+from .models import Order, OrderItem
 from .forms import OrderCreateForm
+from cart.cart import Cart
+from django.conf import settings
+from django.template.loader import render_to_string
+
 
 def order_create(request):
     cart = Cart(request)
@@ -14,6 +19,32 @@ def order_create(request):
                                          price=item['price'], quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # send email
+            name = form.cleaned_data['first_name']
+            surname = form.cleaned_data['last_name']
+            username_email = form.cleaned_data['email']
+            address = form.cleaned_data['address']
+            postal_code = form.cleaned_data['postal_code']
+            city = form.cleaned_data['city']
+            code = order.code
+            subject = 'Gracias por tu pedido!'
+
+            total_cost = order.get_total_cost()
+            if order.payment_method == 'contrareembolso':
+                total_cost += 5
+
+            email = EmailMessage(
+                subject,
+                f'Gracias por tu pedido, {name} {surname}.\n\n'
+                f'Hemos recibido tu pedido con el código {code}.\n\n'
+                f'Con un importe de {total_cost}€.\n\n'
+                f'Dirección de envío:\n{address}, {postal_code} {city}\n'
+                'Si tienes alguna duda, por favor, contacta con nosotros en AcademiaTerminus@cleon.com',
+                to=[username_email]
+            )
+            email.fail_silently = False
+            email.send()
+            messages.success(request, 'Pedido realizado con éxito')
             return render(request, 'order/created.html', {'order': order})
     else:
         form = OrderCreateForm(user=request.user)
@@ -25,9 +56,13 @@ def search(request):
         order_code = request.POST.get("order_code")
         try:
             order = Order.objects.get(code=order_code)
+            total_price = order.get_total_cost()
+            if order.payment_method == 'contrareembolso':
+                total_price += 5
             context['order'] = order
             context['items'] = order.items.all()
-        except:          
+            context['total_price'] = total_price
+        except Order.DoesNotExist:          
             context['error'] = "No existe pedido con dicho localizador"
 
     return render(request, "order/search.html", context)
