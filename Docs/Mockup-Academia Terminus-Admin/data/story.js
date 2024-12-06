556e00d2e2505e3543555980912718f10ca000bd
pabcascom <pabcascom@alum.us.es>
2024-12-06 17:55:28 +0100

    documentos actualizados para la entrega final

---
+++ Docs/Mockup-Academia Terminus-Admin/data/story.js
@@ -0,0 +1,678 @@
+var story = {
+ "docName": "PGPI",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "PGPI",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "50:319",
+   "groupIndex": 0,
+   "title": "LISTA CURSOS",
+   "image": "lista-cursos.png",
+   "index": 0,
+   "width": 1280,
+   "height": 720,
+   "x": -2147,
+   "y": -1080,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 9",
+     "rect": {
+      "x": 105,
+      "y": 143,
+      "width": 182,
+      "height": 226
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "ACADEMIA TERMINUS",
+     "rect": {
+      "x": 352,
+      "y": 12,
+      "width": 576,
+      "height": 52
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 59",
+     "rect": {
+      "x": 1207,
+      "y": 10,
+      "width": 53,
+      "height": 53
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "50:394",
+   "groupIndex": 0,
+   "title": "PAGINA DE CADA PRODUCTO",
+   "image": "pagina-de-cada-producto.png",
+   "index": 1,
+   "width": 1280,
+   "height": 720,
+   "x": -2147,
+   "y": 360,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "ACADEMIA TERMINUS",
+     "rect": {
+      "x": 352,
+      "y": 12,
+      "width": 576,
+      "height": 52
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 57",
+     "rect": {
+      "x": 10,
+      "y": 8,
+      "width": 60,
+      "height": 54.135337829589844
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 59",
+     "rect": {
+      "x": 1207,
+      "y": 10,
+      "width": 53,
+      "height": 53
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "48:2",
+   "groupIndex": 0,
+   "title": "HOME MENU",
+   "image": "home-menu.png",
+   "index": 2,
+   "width": 1280,
+   "height": 720,
+   "x": -640,
+   "y": -360,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "image 55",
+     "rect": {
+      "x": 1207,
+      "y": 10,
+      "width": 53,
+      "height": 53
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Rectangle 11",
+     "rect": {
+      "x": 733,
+      "y": 185,
+      "width": 268,
+      "height": 350
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 60",
+     "rect": {
+      "x": 279,
+      "y": 257,
+      "width": 325,
+      "height": 214
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "50:250",
+   "groupIndex": 0,
+   "title": "PAGINA USUARIO",
+   "image": "pagina-usuario.png",
+   "index": 3,
+   "width": 1280,
+   "height": 720,
+   "x": -640,
+   "y": 720,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "ACADEMIA TERMINUS",
+     "rect": {
+      "x": 352,
+      "y": 12,
+      "width": 576,
+      "height": 52
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 57",
+     "rect": {
+      "x": 11,
+      "y": 9,
+      "width": 60,
+      "height": 54.135337829589844
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "48:113",
+   "groupIndex": 0,
+   "title": "PERFILES DE LOS USUARIOS",
+   "image": "perfiles-de-los-usuarios.png",
+   "index": 4,
+   "width": 1280,
+   "height": 720,
+   "x": 965,
+   "y": -1080,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "ACADEMIA TERMINUS",
+     "rect": {
+      "x": 352,
+      "y": 12,
+      "width": 576,
+      "height": 52
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 55",
+     "rect": {
+      "x": 1209,
+      "y": 10,
+      "width": 53,
+      "height": 53
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Nombre",
+     "rect": {
+      "x": 231,
+      "y": 127,
+      "width": 182,
+      "height": 63
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 65",
+     "rect": {
+      "x": 78,
+      "y": 92,
+      "width": 134,
+      "height": 134
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "48:95",
+   "groupIndex": 0,
+   "title": "PAGINA USUARIO(1)",
+   "image": "pagina-usuario(1).png",
+   "index": 5,
+   "width": 1280,
+   "height": 720,
+   "x": 965,
+   "y": 360,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "ACADEMIA TERMINUS",
+     "rect": {
+      "x": 352,
+      "y": 12,
+      "width": 576,
+      "height": 52
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 55",
+     "rect": {
+      "x": 1209,
+      "y": 10,
+      "width": 53,
+      "height": 53
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 3,
+       "disableAutoScroll": true,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 66",
+     "rect": {
+      "x": 109,
+      "y": 459,
+      "width": 277,
+      "height": 92
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "50:222",
+   "groupIndex": 0,
+   "title": "COMPRAS DEL USUARIO",
+   "image": "compras-del-usuario.png",
+   "index": 6,
+   "width": 1280,
+   "height": 720,
+   "x": 2604,
+   "y": 360,
+   "awidth": 1280,
+   "aheight": 720,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "ACADEMIA TERMINUS",
+     "rect": {
+      "x": 352,
+      "y": 12,
+      "width": 576,
+      "height": 52
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 55",
+     "rect": {
+      "x": 1209,
+      "y": 10,
+      "width": 53,
+      "height": 53
+     },
+     "index": 19,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "image 57",
+     "rect": {
+      "x": 11,
+      "y": 7,
+      "width": 60,
+      "height": 54.135337829589844
+     },
+     "index": 20,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "50:293",
+   "groupIndex": 0,
+   "title": "Group 22",
+   "image": "===group-22.png",
+   "index": 7,
+   "width": 496,
+   "height": 132,
+   "x": 2927,
+   "y": 455,
+   "awidth": 496,
+   "aheight": 132,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "startPageIndex": 2,
+ "totalImages": 8,
+ "groups": [
+  {
+   "id": "41:348",
+   "index": 0,
+   "name": "ADMIN",
+   "backColor": "#F5F5F5"
+  }
+ ]
+}
